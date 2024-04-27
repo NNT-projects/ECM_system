787b4cc455803fbf889a5bb18292ba00293f1410
Ermakov Yaroslav <pengven357@gmail.com>
2024-04-27 13:56:49 +0300

    added database connection and data response

--- db_create.py
+++ db_create.py
@@ -1,41 +1,13 @@
 import psycopg2
-import os
-import csv
 
 try:
     connection = psycopg2.connect(
-        dbname=os.getenv('POSTGRES_DB', 'testDB'),
-        user=os.getenv('POSTGRES_USER', 'user'),
-        host="localhost",
-        password=os.getenv('POSTGRES_PASSWORD', '123'),
-        port=os.getenv('POSTGRES_PORT', '5432')
+        dbname='testDB',
+        user='postgres',
+        host='localhost',
+        password='1234',
+        port='5432'
     )
-
-    # # Создание курсора для выполнения SQL-запросов
-    # cursor = connection.cursor()
-
-    # # Создание таблицы в базе данных
-    # create_table_query = '''CREATE TABLE IF NOT EXISTS my_table (
-    #                         id SERIAL PRIMARY KEY,
-    #                         field1 VARCHAR(255),
-    #                         field2 VARCHAR(255)
-    #                     );'''
-    # cursor.execute(create_table_query)
-    # connection.commit()
-
-    # # Вставка пары значений в таблицу
-    # insert_query = """INSERT INTO my_table (field1, field2) VALUES (%s, %s);"""
-    # record_to_insert = ('значение1', 'значение2')
-    # cursor.execute(insert_query, record_to_insert)
-    # connection.commit()
-
-    # # Закрытие курсора и соединения
-    # cursor.close()
-    # connection.close()
-
-    # print("Запись успешно добавлена в базу данных.")
-
-
     csv_file_path = 'C:/Users/pengv/Downloads/X_train.csv'
 
     connection.autocommit = True
@@ -46,30 +18,6 @@ try:
     with open(csv_file_path, 'r', encoding="utf-8") as f:
         next(f)
         cur.copy_from(f, table_name, sep=',')
-
-    '''
-    columns = '"reportts" TIMESTAMP, "acnum" VARCHAR(255), "pos" INTEGER, "fltdes" INTEGER, "dep" VARCHAR(255), "arr" VARCHAR(255)'
-    with open(csv_file_path, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        headers = next(reader)
-        count = 0
-        for header in headers:
-            if count < 6:
-                count += 1
-                continue
-            columns += f', "{header}" REAL'
-    
-    #with connection.cursor() as cursor:
-    #    cursor.execute("SELECT VERSION();")
-    #    print(f"Server version: {cursor.fetchone()}")
-
-
-    with connection.cursor() as cursor:
-        cursor.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({columns})')
-
-    connection.close()
-    print("Connection closed")
-    '''
 
 except Exception as _ex:
     print('Exception occurred:', _ex)
--- main.py
+++ main.py
@@ -1,55 +1,19 @@
 from datetime import datetime
 from typing import Optional, List
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
-import os
-
-from fastapi import FastAPI, Query
-from fastapi.params import Path
-from pydantic import BaseModel
 
 app = FastAPI()
 
-users = []
-items = []
 
-db_name = os.getenv('POSTGRES_DB', 'testDB')
-
-connection = psycopg2.connect(
-    dbname=os.getenv('POSTGRES_DB', 'testDB'),
-    user=os.getenv('POSTGRES_USER', 'user'),
-    host=os.getenv('POSTGRES_HOST', 'project-db'),
-    password=os.getenv('POSTGRES_PASSWORD', '123'),
-    port=os.getenv('POSTGRES_PORT', '5432')
-)
-
-cursor = connection.cursor()
-
-create_table_query = '''CREATE TABLE IF NOT EXISTS my_table (
-                            id SERIAL PRIMARY KEY,
-                            field1 VARCHAR(255),
-                            field2 VARCHAR(255)
-                        );'''
-
-cursor.execute(create_table_query)
-connection.commit()
-
-# Вставка пары значений в таблицу
-insert_query = f"INSERT INTO my_table (field1, field2) VALUES (%s, %s);"
-record_to_insert = ('значение1', 'значение2')
-cursor.execute(insert_query, record_to_insert)
-connection.commit()
-
-# Закрытие курсора и соединения
-cursor.close()
-connection.close()
-
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
+password = "1234"
+database_name = "testDB"
+DB_table = "data_engine"
+username = "postgres"
 
 
 class Item(BaseModel):
@@ -57,34 +21,32 @@ class Item(BaseModel):
     time_start: datetime
     time_end: datetime
 
+
 @app.post("/items")
 async def request_in_db(item: Item):
+    connection = psycopg2.connect(
+        dbname='testDB',
+        user='postgres',
+        host='localhost',
+        password='1234',
+        port='5432',
+        cursor_factory=RealDictCursor
+    )
+    cursor = connection.cursor()
 
-    return "SELECT " + item.parameters + " FROM " + db_name
-
-
-@app.get("/items", response_model=List[Item])
-async def return_items():
-    return items
-
-
-@app.get("/users", response_model=List[User])
-async def root():
-    return users
-
-
-@app.post("/users")
-async def create_user(user: User):
-    users.append(user)
-    return "Successfully created"
-
-
-@app.get("/users/{id}")
-async def get_user(id: int = Path(..., description="The ID of user you want to get"),
-                   q: str = Query(None, max_length=5)):
-    return {"user": users[id], "query": q}
+    cursor.execute(f"SELECT reportts, {item.parameters}" +
+                   " " +
+                   f"FROM {DB_table}" +
+                   " " +
+                   f"WHERE reportts >= '{item.time_start}' and reportts <= '{item.time_end}'" +
+                   " " +
+                   f"ORDER BY reportts")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    response = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    return response
+
+
