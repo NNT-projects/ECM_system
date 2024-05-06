d7a70a5032b9e34425fd1752f7b7f3503567b56a
motya <motayzz32@gmail.com>
2024-05-06 21:03:38 +0300

    db_create script fix

--- db_create.py
+++ db_create.py
@@ -1,4 +1,5 @@
 import psycopg2
+import csv
 from params_for_DB import *
 
 try:
@@ -12,14 +13,21 @@ try:
 
     connection.autocommit = True
 
+    cur = connection.cursor()
 
-    cursor = connection.cursor()
-    with open(csv_file_path, 'r', encoding="utf-8") as file:
-        next(file)
-        cursor.copy_from(file, table_name, sep=',')
+    with open(csv_file_path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        columns = ', '.join([f'"{header}" VARCHAR(255)' for header in headers])
+        print(columns)
 
-        cursor.execute("UPDATE " + table_name + " SET egtm = 0;")
+    cur.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({columns})')
+
+    with open(csv_file_path, 'r', encoding='utf-8') as f:
+        next(f)  # Skip header
+        cur.copy_from(f, table_name, sep=',', null='')
+
+    cur.execute(f'ALTER TABLE {table_name} ADD COLUMN IF NOT EXISTS "egtm" VARCHAR(255)')
 
 except Exception as _ex:
     print('Exception occurred:', _ex)
-
